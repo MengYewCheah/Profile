61f086327d52c9261441195a2661d971ca7c2544
CheahMengYew <MengYew.CHEAH@3ds.com>
2024-01-09 17:45:49 +0800

    Added Movie Commentary

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,6 +3,7 @@ import { Card, CardHeader, CardBody, CardImg, CardText } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ProfileImage from '../assets/profile-pic.png';
+import RanPoster from '../assets/ranPoster.jpg';
 
 function Home() {
   return (
@@ -20,7 +21,7 @@ function Home() {
               </CardText>
 
               <CardTitle style={{paddingLeft: "30%"}}><h3 style={{fontWeight: "bolder"}}>Bio - Data</h3></CardTitle>
-              <CardText style={{marginLeft: "-10px"}}>
+              <CardText style={{marginLeft: "14%"}}>
                 <ul>
                   <li style={{listStyleType: "none"}}>Name : Cheah Meng Yew</li>
                   <li style={{listStyleType: "none"}}>Age : 25</li>
@@ -38,7 +39,21 @@ function Home() {
             </CardBody>
           </Card>
         </Col>
-        <Col>
+        <Col className="col-12 col-md-9">
+          <Card className="p-3 mb-2 w-100 bg-white text-dark" style={{margin: "0"}}>
+            <CardHeader>
+            <CardTitle ><h3 style={{fontWeight: "bolder"}}>This Week Movie Commentary</h3></CardTitle>
+            </CardHeader>
+            <CardBody>
+              <Row>
+                <Col><CardImg src={RanPoster}  height={550}></CardImg></Col>
+                <Col className="col-12 col-md-6">
+                Ran : One of the best story telling by the late Akira Kurosawa, some even say the best. This movie is a must watch for samurai fans. It tells a story about an old warlord dividing his serdom to his three sons that eventually corrupts them. It was based on the story of King Leer by Shakespear. The method the movie was protrayed allows us to relate to the characters and feel what they felt. The amazing protrayal of Tatsuya Nakadai as Hidetora Ichimonji shows us how the warlord turn to Insanity. Overall I will give it a 10/10.
+
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
         </Col>
       </Row>
     </Container>
