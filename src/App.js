724df0c3a21812c9ef561b5af084cb6acc206ed9
CheahMengYew <MengYew.CHEAH@3ds.com>
2024-01-09 13:40:52 +0800

    Added Navigational Bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import About from './routes/About';
-import Navbar from './routes/Navbar';
+import Navigation from './routes/Navbar';
 import Home from './routes/Home';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navigation/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/routes/Navbar.js
+++ src/routes/Navbar.js
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
+const Navigation = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand>Cheah Meng Yew</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link to="/about"><Link to="/about">About</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link> */}
+      </Container>
+    </Navbar>
   )
 }
 
-export default Navbar;
+export default Navigation;
